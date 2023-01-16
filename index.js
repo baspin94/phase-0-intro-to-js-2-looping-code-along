b7e88df05e988945fedb74c3e3f589c6370c68d0
Bianca Aspin <bianca@mj94.net>
2023-01-15 19:14:21 -0800

    Completed lab (Phase 1 review)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArray, eventName) {
+    let completedCards = [];
+    for(let i = 0; i < nameArray.length; i++){
+        let message = `Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`
+        completedCards.push(message);
+    }
+    return completedCards;
+};
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
